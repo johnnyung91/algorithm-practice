2db24fc13ef4e84db2c887c62b4a3a037be1067e
Johnny Ung <johnnyung91@gmail.com>
2020-05-18 11:59:52 -0700

    added other solutions to practice 10

--- codesignal/practice 10 - commonCharacterCount/main.js
+++ codesignal/practice 10 - commonCharacterCount/main.js
@@ -15,3 +15,27 @@ function commonCharacterCount(s1, s2) {
 
   return count;
 }
+
+
+/* other solutions:
+
+function commonCharacterCount(s1, s2) {
+    for (var i = 0; i < s1.length; i++) {
+        s2 = s2.replace(s1[i], "!");
+    }
+    return s2.replace(/[^!]/g, "").length;
+}
+
+function commonCharacterCount(s1, s2) {
+  var a=s1.split(""),r=0
+  while (a.length){
+    var t=a.pop()
+    if(s2.includes(t)){
+      r++
+      s2=s2.replace(t,"")
+    }
+  }
+  return r
+}
+
+*/
