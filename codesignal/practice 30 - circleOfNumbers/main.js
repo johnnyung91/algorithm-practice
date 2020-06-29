e56494655f2c8879b9c4e820be5274791fa47977
Johnny Ung <johnnyung91@gmail.com>
2020-06-29 19:57:10 +0000

    cs practice 30 completed

---
+++ codesignal/practice 30 - circleOfNumbers/main.js
@@ -0,0 +1,25 @@
+function circleOfNumbers(n, firstNumber) {
+  return (n / 2 + firstNumber) % n
+}
+
+
+/* Other Solutions
+
+function circleOfNumbers(n, firstNumber) {
+  return firstNumber >= n / 2
+    ? firstNumber - n / 2
+    : firstNumber + n / 2
+}
+
+function circleOfNumbers(n, firstNumber) {
+  const middle = (n / 2) - 1;
+  if (firstNumber <= middle) {
+    return ((n - 2) / 2) + firstNumber + 1;
+  } else {
+    return firstNumber - ((n - 2) / 2) - 1;
+  }
+}
+
+
+
+*/
