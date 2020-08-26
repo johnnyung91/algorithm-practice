335efde7a53fcfef2048e3c95937e67b7ae6683d
Johnny Ung <johnnyung91@gmail.com>
2020-08-26 06:07:58 +0000

    added a solution without converting string to a number

--- leetcode/9 - palindromeNumber/isPalindrome.js
+++ leetcode/9 - palindromeNumber/isPalindrome.js
@@ -2,3 +2,18 @@ const isPalindrome = x => {
     const string = x.toString()
     return string === string.split('').reverse().join('')
 };
+
+const isPalindrome2 = x => {
+    if (x === 0) return true
+    if (x < 0 || x % 10 === 0) return false
+
+    let reverse = 0
+    let original = x
+
+    while (x > 0) {
+        reverse = reverse * 10 + x % 10
+        x = parseInt(x/10, 10)
+    }
+
+    return original === reverse
+}
