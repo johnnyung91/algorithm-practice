74b6142c1913099764c5c32096d212a089f8fa45
Johnny Ung <johnnyung91@gmail.com>
2020-08-29 21:27:06 +0000

    added two pointer method

--- leetcode/11 - container with most water/maxArea.js
+++ leetcode/11 - container with most water/maxArea.js
@@ -7,3 +7,18 @@ const maxArea = function(height) {
     }
     return maxArea
 };
+
+
+const maxArea2 = function(height) {
+    let maxArea = 0;
+    let left = 0;
+    let right = height.length - 1
+
+    while (left < right) {
+        maxArea = Math.max(maxArea, Math.min(height[left], height[right]) * (right - left))
+        if (height[left] < height[right]) left++
+        else (right--)
+    }
+
+    return maxArea
+}
