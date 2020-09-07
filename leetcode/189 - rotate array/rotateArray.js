679ad0ec77f401d05258d54dcede6ad7991452ad
Johnny Ung <johnnyung91@gmail.com>
2020-09-07 06:47:06 +0000

    added O(n) solution reversing 3 times

--- leetcode/189 - rotate array/rotateArray.js
+++ leetcode/189 - rotate array/rotateArray.js
@@ -6,3 +6,21 @@ const rotate = (nums, k) => {
   }
   return nums
 };
+
+const rotate2 = (nums, k) => {
+    const lastIndex = nums.length-1
+    k %= nums.length
+    reverse(nums, 0, lastIndex)
+    reverse(nums, 0, k-1)
+    reverse(nums, k, lastIndex)
+}
+
+function reverse(array, start, end) {
+    while (start < end) {
+        let temp = array[start]
+        array[start] = array[end]
+        array[end] = temp
+        start++
+        end--
+    }
+}
